d06fcef9e4d62f4a2fe87ec7f7d7e281b5923a11
Maximiliano Paris <mparis@hotmail.cl>
2024-09-24 08:16:52 -0600

    Initial Commit

---
+++ Calculator-lazygrid/Calculator_lazygridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_lazygridApp.swift
+//  Calculator-lazygrid
+//
+//  Created by Maximiliano París Gaete on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_lazygridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator-lazygrid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calculator-lazygrid
+//
+//  Created by Maximiliano París Gaete on 9/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
